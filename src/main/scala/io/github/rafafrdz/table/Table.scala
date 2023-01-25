7b1ac481f2ad7eabe7d52ceae2aabb50b38ee946
rfernandez <rfernandez@stratio.com>
2023-01-25 18:37:29 +0100

    [Add] require

--- src/main/scala/io/github/rafafrdz/table/Table.scala
+++ src/main/scala/io/github/rafafrdz/table/Table.scala
@@ -24,12 +24,15 @@ case class Table(properties: Properties) {
 
   private lazy val header: Array[String] = {
     val h: Array[String] = df.columns
+    lazy val wordsBlank = h.filter(s => s != s.trim)
     require(h.length > 1, "Table must have more than one column")
+    require(h.forall(s => s == s.trim), s"Columns' name have a blank space outside the word. ${wordsBlank.map(w => s"'$w' =/= '${w.trim}'").mkString(", ")}")
     h
   }
 
   private lazy val to: String = {
-    Try(header.filter(w => w.toLowerCase == "to").head) match {
+    val hd: Array[String] = header
+    Try(hd.filter(w => w.toLowerCase == "to").head) match {
       case Failure(_) => throw new Exception("Table must have the column 'to' in its header")
       case Success(value) => value
     }
